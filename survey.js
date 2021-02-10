bd0fab2e84faafc7080ff3339db58f81485f5db1
Saïd Redouane <s_red009@hotmail.com>
2021-02-10 01:08:42 +0000

    Created the survey.js function (i.e. callback hell function)

---
+++ survey.js
@@ -0,0 +1,62 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let paragraph = "";
+
+rl.question(
+  "What's your name? Nicknames are also acceptable :)? ",
+  (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+    let name = answer;
+    rl.question("What's an activity you like doing?", (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      let activity = answer;
+      rl.question("What do you listen to while doing that?", (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+        let music = answer;
+        rl.question(
+          "Which meal is your favourite (eg: dinner, brunch, etc.)",
+          (answer) => {
+            console.log(`Thank you for your valuable feedback: ${answer}`);
+            let meal = answer;
+            rl.question(
+              "What's your favourite thing to eat for that meal?",
+              (answer) => {
+                console.log(`Thank you for your valuable feedback: ${answer}`);
+                let food = answer;
+
+                rl.question(
+                  "Which sport is your absolute favourite?",
+                  (answer) => {
+                    console.log(
+                      `Thank you for your valuable feedback: ${answer}`
+                    );
+                    let sport = answer;
+
+                    rl.question(
+                      "What is your superpower? In a few words, tell us what you are amazing at!",
+                      (answer) => {
+                        console.log(
+                          `Thank you for your valuable feedback: ${answer}`
+                        );
+                        let superPower = answer;
+                        console.log(
+                          `${name} loves listening to ${music} while ${activity}, devouring ${food} for ${meal}, prefers ${sport} over any other sport, and is amazing at ${superPower} `
+                        );
+                        rl.close();
+                      }
+                    );
+                  }
+                );
+              }
+            );
+          }
+        );
+      });
+    });
+  }
+);
